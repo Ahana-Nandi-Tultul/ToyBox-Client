d8f98dee347da94d11056e85e1bd195cbe57b5ef
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-25 23:33:47 +0600

    delete toys from MyToys

--- src/pages/MyToys/MyToyRow.jsx
+++ src/pages/MyToys/MyToyRow.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const MyToyRow = ({toy}) => {
-    const {toyName, toyPrice, quantity, subCategory, photo, ratting, description} = toy;
+const MyToyRow = ({toy, handleDeleteToy}) => {
+    const {_id, toyName, toyPrice, quantity, subCategory, photo, ratting, description} = toy;
     return (
         <tr>
             <th>
-            
-                <button className="btn btn-circle btn-outline">
+                <button className="btn btn-circle btn-outline" onClick={() => handleDeleteToy(_id)}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
             
--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 import MyToyRow from './MyToyRow';
+import Swal from 'sweetalert2';
 
 const MyToys = () => {
     const {user} = useContext(AuthContext);
@@ -12,6 +13,37 @@ const MyToys = () => {
         .then(data => setToys(data))
 
     },[user])
+
+    const handleDeleteToy = id => {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:3000/toys/${id}?email=${user?.email}`, {
+            method: "DELETE"
+          })
+          .then(res => res.json())
+          .then(data => {
+            if(data.deletedCount > 0){
+              console.log(data)
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+              const remaining = toys.filter(toy => toy._id !== id)
+              setToys(remaining)
+            }
+          })
+        }
+      })
+    }
 
     return (
         <div className='my-16 md:w-5/6 px-4 mx-auto'>
@@ -39,6 +71,7 @@ const MyToys = () => {
                   toys.map(toy => <MyToyRow
                   key={toy._id}
                   toy={toy}
+                  handleDeleteToy = {handleDeleteToy}
                   ></MyToyRow>)
                 }
               </tbody>
