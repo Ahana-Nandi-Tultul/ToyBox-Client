b8cace5b6e07830c32264fc77e93a9d914134477
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-26 14:10:09 +0600

    fix navber, add user img and tooltip

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import router from './routes/Routes.jsx';
 import AuthProvider from './providers/AuthProvider';
+import 'react-tooltip/dist/react-tooltip.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
--- src/pages/shared/Navbar/Navber.jsx
+++ src/pages/shared/Navbar/Navber.jsx
@@ -2,9 +2,12 @@ import React, { useContext } from 'react';
 import logo from '../../../assets/logo.png';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../providers/AuthProvider';
+import { FaUserCircle } from 'react-icons/fa';
+import { Tooltip } from 'react-tooltip'
 
 const Navber = () => {
     const {user, logout} = useContext(AuthContext);
+    // console.log(user);
     const handleLogout = () => {
         logout()
         .then(() => {})
@@ -14,8 +17,12 @@ const Navber = () => {
     const navLink = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/allToys">All Toys</Link></li>
-        <li><Link to="/myToys">My Toys</Link></li>
-        <li><Link to="/addAToy">Add A Toy</Link></li>
+        {
+            user && <>
+             <li><Link to="/myToys">My Toys</Link></li>
+            <li><Link to="/addAToy">Add A Toy</Link></li>
+            </>
+        }
     </>
     return (
         <div className="navbar bg-base-100 md:px-10">
@@ -24,8 +31,31 @@ const Navber = () => {
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 
+                shadow bg-base-100 rounded-box w-52 flex lg:hidden">
                     {navLink}
+                    <div className="navbar-end menu menu-sm">
+                    {
+                    user ? <>
+                    <div className="avatar mr-4 mb-2">
+                        <div className="w-10 h-10 rounded-full ring ring-[#f5b48e] ring-offset-2">
+                            {
+                                user?.photoURL ? 
+                                <img src={user?.photoURL} 
+                                className='me-2 bg-dark text-white' 
+                                data-tooltip-id="my-tooltip" data-tooltip-content={user?.displayName}/>
+                                :<FaUserCircle data-tooltip-content={user?.displayName}/>
+                            }
+                            <Tooltip id="my-tooltip" className='bg-black text-white z-20' />
+
+                        </div>
+                    </div>
+                    <button className='btn' onClick={handleLogout}>Logout</button>         
+                    </> 
+                    :<Link to="/login" className="btn">Login</Link>
+                }
+                
+            </div>
                 </ul>
                 </div>
                 <img src={logo} alt="" />
@@ -35,9 +65,21 @@ const Navber = () => {
                     {navLink}
                 </ul>
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end hidden lg:flex">
                 {
                     user ? <>
+                    <div className="avatar mr-4">
+                        <div className="w-10 h-10 rounded-full ring ring-[#f5b48e] ring-offset-2">
+                            {
+                                user?.photoURL ? 
+                                <img src={user?.photoURL} 
+                                className='me-2 bg-dark text-white' 
+                                data-tooltip-id="my-tooltip" data-tooltip-content={user?.displayName}/>
+                                :<FaUserCircle data-tooltip-content={user?.displayName}/>
+                            }
+                           <Tooltip id="my-tooltip" className='bg-black text-white z-20' />
+                        </div>
+                    </div>
                     <button className='btn' onClick={handleLogout}>Logout</button>         
                     </> 
                     :<Link to="/login" className="btn">Login</Link>
